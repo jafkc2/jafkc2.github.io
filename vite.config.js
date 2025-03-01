e97fcae18bbfcd191b66cc0b85ebc8ae90b588fe
jafkc2 <jaf222006ar@gmail.com>
2025-02-28 23:18:41 -0300

    Configurar vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineConfig({
+  build: {
+    target: 'es2022',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        kc_overlay: resolve(__dirname, 'projetos/kc-overlay.html'),
+        minelander: resolve(__dirname, 'projetos/minelander.html'),
+        showcase: resolve(__dirname, 'projetos/showcase.html'),
+
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
