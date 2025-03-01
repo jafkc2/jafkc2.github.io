c4fc3c2f011178317c2fd234a578b45763e0d85d
jafkc2 <jaf222006ar@gmail.com>
2025-02-28 23:05:51 -0300

    Adiciona os projetos

--- src/main.ts
+++ src/main.ts
@@ -15,22 +15,22 @@ const body = html`
     <div class="bg-surface p-6 rounded-xl shadow-lg">
         <h2 class="text-xl font-semibold">Site de showcase</h2>
         <p class="text-gray-400 mt-2">Este site que você está vendo 🙂. Site estático simples feito com HTML, TailwindCSS e Typescript.</p>
-        <a href="showcase" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
+        <a href="../projetos/showcase" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
     </div>
     <div class="bg-surface p-6 rounded-xl shadow-lg">
       <h2 class="text-xl font-semibold">KC Overlay</h2>
       <p class="text-gray-400 mt-2">Ferramenta para jogadores do servidor de Minecraft MushMC conseguirem ver as estatísticas dos jogadores.</p>
-      <a href="kc-overlay" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
+      <a href="../projetos/kc-overlay" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
     </div>
     <div class="bg-surface p-6 rounded-xl shadow-lg">
       <h2 class="text-xl font-semibold">Minelander</h2>
       <p class="text-gray-400 mt-2">Launcher de Minecraft feito em Rust. Possui diversas funcionalidades interessantes.</p>
-      <a href="minelander" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
+      <a href="../projetos/minelander" class="text-blue-400 hover:underline mt-4 block">Ver projeto</a>
     </div>
   </section>
 
   <footer class="text-center py-10 mt-10 text-gray-500">
-    <a href ="https://github.com/jafkc2" target="_black">
+    <a href ="https://github.com/jafkc2" target="_black" class="text-mylightblue">
       Meu Github
     </a>
     <p>Email: jaf222006@gmail.com</p>
@@ -40,4 +40,3 @@ const body = html`
 `
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = body
-
